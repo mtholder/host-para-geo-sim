12811e3a2a830a0a74377bb14d8982553192db41
Mark T. Holder <mtholder@gmail.com>
2014-01-16 22:29:19 -0600

    sum of host branch len stat

--- summary_stats.py
+++ summary_stats.py
@@ -23,21 +23,24 @@ for k, v in p2h.items():
     val_list = [h_taxa.get_taxon(label=i) for i in v]
     taxa_p2h[p_taxa.get_taxon(label=k)] = val_list
 
+def calculate_mean_sum_of_tree_length_covered(taxa_p2h, h_tree):
+    t0 = 0.0
+    c = 0
+    for k, v in taxa_p2h.items():
+        c += 1
+        if len(v) > 1:
+            mrca_root = h_tree.mrca(taxa=v)
+            d = {}
+            for n in v:
+                curr_nd = h_tree.find_node_with_taxon(lambda x: x == n)
+                while (curr_nd is not mrca_root) and (curr_nd.taxon not in d):
+                    d[curr_nd.taxon] = curr_nd.edge.length
+                    curr_nd = curr_nd.parent_node
+            s = sum([v for v in d.values()])
+            t0 += s
+    return t0/c
+
 h_len = h_tree.length()
 print h_len
-t0 = 0.0
-num_para = len(p_taxa)
-for k, v in taxa_p2h.items():
-    if len(v) > 1:
-        mrca_root = h_tree.mrca(taxa=v)
-        d = {}
-        for n in v:
-            curr_nd = h_tree.find_node_with_taxon(lambda x: x == n)
-            while (curr_nd is not mrca_root) and (curr_nd.taxon not in d):
-                d[curr_nd.taxon] = curr_nd.edge.length
-                curr_nd = curr_nd.parent_node
-        s = sum([v for v in d.values()])
-        t0 += s/h_len
-t0 /= num_para
-    
+t0 = calculate_mean_sum_of_tree_length_covered(taxa_p2h, h_tree)
 print t0
